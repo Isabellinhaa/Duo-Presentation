efd16f139ced015e7637db37811bff399c529e49
Anderson-Armando <arevalosanderson9@gmail.com>
2024-06-17 20:20:44 -0300

    Apresentação Finalizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitialScreenComponent } from './initial-screen/initial-screen.component';
+import { AndersonScreenComponent } from './anderson-screen/anderson-screen.component';
+import { IsabellaScreenComponent } from './isabella-screen/isabella-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InitialScreenComponent},
+  {path: 'anderson-screen', component: AndersonScreenComponent},
+  {path: 'isabella-screen', component: IsabellaScreenComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
